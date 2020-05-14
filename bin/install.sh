2199cb74c32012be4fb6c7633ef9b5800b5d6bf6
YannKNX <yzhao@kernix.com>
2020-05-14 15:57:11 +0200

    update install sh

--- bin/install.sh
+++ bin/install.sh
@@ -3,7 +3,7 @@
 # Copy config files
 cp docker-compose.override.yml.dist docker-compose.override.yml
 cp app/drupal/.env.dist app/drupal/.env
-cp app/drupal/web/sites/default/default.settings.php.dist app/drupal/web/sites/default/settings.php
+#cp app/drupal/web/sites/default/default.settings.php.dist app/drupal/web/sites/default/settings.php
 
 # Build dockers
 docker-compose up -d --build
